21f91acc7b7e645c8deb2dd86869224aec72afa1
wilfred05777@gmail.com <wilfred05777@gmail..com>
2023-03-05 05:42:42 +0800

    77. Setting Up Our Categories

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,81 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      cta: 'Shop Now'
+    },
+    {
+      id: 2,
+      title: 'Jacket',
+      cta: 'Shop Now'
+    },
+
+    {
+      id: 3,
+      title: 'Sneakers',
+      cta: 'Shop Now'
+    },
+    {
+      id: 4,
+      title: 'Women',
+      cta: 'Shop Now'
+    },
+    {
+      id: 5,
+      title: 'Men',
+      cta: 'Shop Now'
+    }
+  ]
+
   return (
     <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
+      {categories.map(({ id, title, cta }) => (
+        <div key={id} className='category-container'>
+          {/* <img /> */}
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>{cta}</p>
+          </div>
+        </div>
+      ))}
+      {/* <div className='category-container'>
+        <img />
         <div className='category-body-container'>
           <h2>Hats</h2>
           <p>Shop Now</p>
         </div>
       </div>
       <div className='category-container'>
-        {/* <img /> */}
+        <img />
         <div className='category-body-container'>
           <h2>Jackets</h2>
           <p>Shop Now</p>
         </div>
       </div>
       <div className='category-container'>
-        {/* <img /> */}
+        <img />
         <div className='category-body-container'>
           <h2>Sneakers</h2>
           <p>Shop Now</p>
         </div>
       </div>
       <div className='category-container'>
-        {/* <img /> */}
+        <img />
         <div className='category-body-container'>
           <h2>Womens</h2>
           <p>Shop Now</p>
         </div>
       </div>
       <div className='category-container'>
-        {/* <img /> */}
+        <img />
         <div className='category-body-container'>
           <h2>Mens</h2>
           <p>Shop Now</p>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
