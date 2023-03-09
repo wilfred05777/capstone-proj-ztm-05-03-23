bd969dc61e8bc0d36bb14609c636de40cff3117a
wilfred05777@gmail.com <wilfred05777@gmail..com>
2023-03-09 18:59:12 +0800

    85. React Router Outlet and with note at readme.me

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,22 @@ import './App.css'
 
 import Home from './routes/home/home.components'
 
+const Shop = () => {
+  return <h1>I am the shop page</h1>
+}
+
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      {/* <Home /> */}
+      <Route path='/home' element={<Home />}>
+        <Route path='shop' element={<Shop />} />
+        {/* /home/shop */}
+      </Route>
+      {/* code above - it is No longer a sibling but nested element -> Parent level add a <Outlet/> to render the nested which is /shop, because it will only render the parent level element. */}
+
+      {/* <Route path='/' element={<Home />} /> */}
+      {/* <Route path='/shop' element={<Shop />} /> */}
     </Routes>
-    // <Directory categories={categories} />
   )
 }
 
--- src/routes/home/home.components.jsx
+++ src/routes/home/home.components.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Directory from '../../components/directory/directory.component'
+import { Outlet } from 'react-router-dom'
 
 const Home = () => {
   const categories = [
@@ -36,6 +37,7 @@ const Home = () => {
   ]
   return (
     <>
+      <Outlet />
       <Directory categories={categories} />
     </>
   )
