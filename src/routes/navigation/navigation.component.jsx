6f046f9a6539fd5ce1bf05df8e874869f40e9fe6
wilfred05777@gmail.com <wilfred05777@gmail..com>
2023-03-10 08:28:45 +0800

    88. Styling for Navigation + Logo

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -5,15 +5,20 @@ import { Fragment } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 
 /**
+ * Vite and SVG rendering for navigation fixes and issues
+ *
  * BUG on import { ReactComponent as CrwnLogo } from '/src/assets/images/crown.svg'
+ *
  *
  * https://stackoverflow.com/questions/70309561/unable-to-import-svg-with-vite-as-reactcomponent
  *
- *
+ * !Solution!: just follow the link below to fix the issue
  * https://www.npmjs.com/package/vite-plugin-svgr
+ *
+ *
  */
 
-import { ReactComponent as CrwnLogo } from '/src/assets/images/crown.svg'
+import { ReactComponent as Logo } from '../../assets/images/crown.svg'
 
 import './navigation.styles.scss'
 
@@ -23,9 +28,9 @@ const Navigation = () => {
     <Fragment>
       <div className='navigation'>
         <Link className='logo-container' to={'/'}>
-          <CrwnLogo className='logo' />
+          <Logo className='logo' />
         </Link>
-        <div className='links-container'>
+        <div className='nav-links-container'>
           {/* Link behaves like an anchor tag link in a plain html */}
           <Link className='nav-link' to={'/shop'}>
             Shop
