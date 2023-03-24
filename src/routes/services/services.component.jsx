a77b7d0c31507b203492fd4a59be525a615c1a5b
wilfred05777@gmail.com <wilfred05777@gmail..com>
2023-03-24 16:01:12 +0800

    bug

--- src/routes/services/services.component.jsx
+++ src/routes/services/services.component.jsx
@@ -7,23 +7,26 @@ const Services = () => {
   const { services } = useContext(ServicesContext)
 
   return (
-    /**with react useContext api**/
-    // <div>
-    //   {services.map(({ id, services }) => (
-    //     <div key={id}>
-    //       <h2>{services}</h2>
-    //     </div>
-    //   ))}
-    // </div>
+    /**with react useContext api
+     * bug at the moment
+     * **/
 
-    /**without react useContext api**/
     <div>
-      {SERVICES_DATA.map(({ id, service }) => (
+      {services.map(({ id, services }) => (
         <div key={id}>
-          <h2>{service}</h2>
+          <h2>{services}</h2>
         </div>
       ))}
     </div>
+
+    /**without react useContext api**/
+    // <div>
+    //   {SERVICES_DATA.map(({ id, service }) => (
+    //     <div key={id}>
+    //       <h2>{service}</h2>
+    //     </div>
+    //   ))}
+    // </div>
   )
 }
 
