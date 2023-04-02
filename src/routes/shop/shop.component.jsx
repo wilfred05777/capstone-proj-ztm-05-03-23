ef8fc759fc33452963f1ec8e8a823a45c34567de
Wilfred <wilfred05777@gmail.com>
2023-04-02 10:13:27 +0800

    133. Category Preview Component creation

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -14,26 +14,37 @@ import { ProductsContext } from '../../contexts/products.context'
 
 import ProductCard from '../../components/product-card/product-card.component'
 import { CategoriesContext } from '../../contexts/categories.context'
+import CategoryPreview from '../../components/category-preview/category-preview.component'
 
 const Shop = () => {
   // const { products } = useContext(ProductsContext)
   const { categoriesMap } = useContext(CategoriesContext)
 
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className='products-container'>
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
+    /** refactor at lecture 133. Category Preview Component */
+    <div className='shop-container'>
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title]
+        return <CategoryPreview key={title} title={title} products={products} />
+      })}
+    </div>
 
-    /** before refactor */
+    /**  before refactoring 132. Using Our CategoriesMap */
+    // <Fragment>
+    //// one line return  => ( code here is a one line return)
+    //   {Object.keys(categoriesMap).map((title) => (
+    //     <Fragment key={title}>
+    //       <h2>{title}</h2>
+    //       <div className='products-container'>
+    //         {categoriesMap[title].map((product) => (
+    //           <ProductCard key={product.id} product={product} />
+    //         ))}
+    //       </div>
+    //     </Fragment>
+    //   ))}
+    // </Fragment>
+
+    /** before refactor 130. Get Products + Categories From Firestore */
 
     // <div className='products-container'>
     //   {/* {products.map((product) => (
