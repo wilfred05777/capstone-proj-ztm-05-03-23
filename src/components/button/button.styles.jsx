5d040a621081287765de8402a6a3a19343b87e5a
Wilfred <wilfred05777@gmail.com>
2023-04-09 17:40:15 +0800

    adjusting font weight

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -7,12 +7,12 @@ export const BaseButton = styled.button`
   letter-spacing: 0.5px;
   line-height: 50px;
   padding: 0 35px 0 35px;
-  font-size: 15px;
+  font-size: 14px;
   background-color: black;
   color: white;
   text-transform: uppercase;
   font-family: 'Open Sans Condensed';
-  font-weight: bolder;
+  font-weight: 500;
   border: none;
   cursor: pointer;
   display: flex;
