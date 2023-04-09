2b50a66323aacf11f11a05dff2a3516edbf01fcd
Wilfred <wilfred05777@gmail.com>
2023-04-09 17:35:08 +0800

    Styled-Component - Directory Item

---
+++ src/components/directory-item/_140_directory-item.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './directory-item.styles.scss'
+
+// const DirectoryItem = ({ categoryItemComponent }) => {
+// const { id, imageUrl, title, cta } = categoryItemComponent
+const DirectoryItem = ({ category }) => {
+  const { id, imageUrl, title, cta } = category
+  // const CategoryItem = ({ category }) => {
+  //   const { id, imageUrl, title, cta } = category
+
+  return (
+    <div className='directory-item-container'>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className='body'>
+        <h2>{title}</h2>
+        <p>SHOP NOW!</p>
+        {/* <p>{cta}</p> */}
+      </div>
+    </div>
+  )
+}
+
+export default DirectoryItem
--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,27 +1,36 @@
 import React from 'react'
-import './directory-item.styles.scss'
+// import './directory-item.styles.scss'
+import {
+  BackgroundImage,
+  Body,
+  DirectoryItemContainer
+} from './directory-item.styles'
 
-// const DirectoryItem = ({ categoryItemComponent }) => {
-// const { id, imageUrl, title, cta } = categoryItemComponent
 const DirectoryItem = ({ category }) => {
   const { id, imageUrl, title, cta } = category
-  // const CategoryItem = ({ category }) => {
-  //   const { id, imageUrl, title, cta } = category
 
   return (
-    <div className='directory-item-container'>
-      <div
-        className='background-image'
-        style={{
-          backgroundImage: `url(${imageUrl})`
-        }}
-      />
-      <div className='body'>
+    <DirectoryItemContainer>
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body>
         <h2>{title}</h2>
         <p>SHOP NOW!</p>
-        {/* <p>{cta}</p> */}
-      </div>
-    </div>
+      </Body>
+    </DirectoryItemContainer>
+
+    // <div className='directory-item-container'>
+    //   <div
+    //     className='background-image'
+    //     style={{
+    //       backgroundImage: `url(${imageUrl})`
+    //     }}
+    //   />
+    //   <div className='body'>
+    //     <h2>{title}</h2>
+    //     <p>SHOP NOW!</p>
+    //     {/* <p>{cta}</p> */}
+    //   </div>
+    // </div>
   )
 }
 
