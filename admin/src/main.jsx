83961e4d5f4db87a876a3f0b92c46031f89bb87e
wilfred05777@gmail.com <wilfred05777@gmail..com>
2023-04-15 03:46:34 +0800

    How to set up Colors and MUI Light and Dark Mode Themes with Redux Toolkit

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { configureStore } from '@reduxjs/toolkit'
+// import globalReducer from 'state' /** error 36:08-https://www.youtube.com/watch?v=0cPCMIuDk2I&list=WL&index=3&t=7195s */
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
