947a1b83d96ab2a231534ddba6063ecdb17c17b5
Wilfred <wilfred05777@gmail.com>
2023-04-27 04:32:08 +0800

    fixed bug main/index.js gobalReducer state to ./state

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { configureStore } from '@reduxjs/toolkit'
-// import globalReducer from 'state' /** error 36:08-https://www.youtube.com/watch?v=0cPCMIuDk2I&list=WL&index=3&t=7195s */
+// import globalReducer from 'state'
+import globalReducer from './state'
+/** error 36:08-https://www.youtube.com/watch?v=0cPCMIuDk2I&list=WL&index=3&t=7195s */
 import { Provider } from 'react-redux'
+
+const store = configureStore({
+  reducer: globalReducer
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
