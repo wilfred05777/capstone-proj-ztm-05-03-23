3a6d3126f330933ac5e2960e89a5b7dcd3bc2c11
Wilfred <wilfred05777@gmail.com>
2023-04-27 10:59:47 +0800

    bug on implemeting theme in app.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,25 @@
-import react from 'react'
+import react, { useMemo } from 'react'
+// import { CssBaseline, ThemeProvider } from '@mui/material'
+// import { createTheme } from '@mui/material'
+// import { themeSettings } from './theme'
+import { theme } from './_theme'
+import { useSelector } from 'react-redux'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+// import theme from './theme'
 
 function App() {
-  return <div className='app'>Test</div>
+  // const mode = useSelector((state) => state.global.mode)
+  // const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+  return (
+    <div className='app'>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <main>This app is using the dark mode</main>
+        {/* <Button sx={{ bgcolor: '#000' }}>My Button</Button> */}
+      </ThemeProvider>
+    </div>
+  )
 }
 
 export default App
